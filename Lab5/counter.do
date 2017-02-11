552c25f96fda0a5b89677cfdecd773ad63ea2b91
AsicChen <asic.chen@mail.utoronto.ca>
2017-02-11 15:32:58 -0500

    part i working now

--- Lab5/counter.do
+++ Lab5/counter.do
@@ -7,4 +7,5 @@ add wave {/*}
 #first test case
 force {KEY[0]} 0 0, 1 1, 0 2, 1 3, 0 4, 1 5, 0 6, 1 7, 0 8, 1 9, 0 10
 force {SW[1]} 1
-force {SW[0]} 1 0, 1 2
+force {SW[0]} 1 0, 0 2, 1 3
+run 10ns
--- Lab5/counter.v
+++ Lab5/counter.v
@@ -6,7 +6,7 @@ module counter(SW, KEY, HEX0, HEX1);
     	output [6:0] HEX1;
 
 	wire Clock, Clear_b, Enable;
-	reg [7:0] Q;
+	wire [7:0] Q;
 	wire [6:0] a;
 
 	assign Clock = KEY[0];
@@ -14,146 +14,146 @@ module counter(SW, KEY, HEX0, HEX1);
 	assign Enable = SW[1];
 
 	toggle q0(
-		.t(Enable);
-		.clock(Clock);
-		.clear_b(Clear_b);
-		.q(Q[0]);
+		.t(Enable),
+		.clock(Clock),
+		.clear_b(Clear_b),
+		.q(Q[0])
 	);
 
 	toggle q1(
-		.t(a[0]);
-		.clock(Clock);
-		.clear_b(Clear_b);
-		.q(Q[1]);
+		.t(a[0]),
+		.clock(Clock),
+		.clear_b(Clear_b),
+		.q(Q[1])
 	);
 
 	toggle q2(
-		.t(a[1]);
-		.clock(Clock);
-		.clear_b(Clear_b);
-		.q(Q[2]);
+		.t(a[1]),
+		.clock(Clock),
+		.clear_b(Clear_b),
+		.q(Q[2])
 	);
 
 	toggle q3(
-		.t(a[2]);
-		.clock(Clock);
-		.clear_b(Clear_b);
-		.q(Q[3]);
+		.t(a[2]),
+		.clock(Clock),
+		.clear_b(Clear_b),
+		.q(Q[3])
 	);
 
 	toggle q4(
-		.t(a[3]);
-		.clock(Clock);
-		.clear_b(Clear_b);
-		.q(Q[4]);
+		.t(a[3]),
+		.clock(Clock),
+		.clear_b(Clear_b),
+		.q(Q[4])
 	);
 
 	toggle q5(
-		.t(a[4]);
-		.clock(Clock);
-		.clear_b(Clear_b);
-		.q(Q[5]);
+		.t(a[4]),
+		.clock(Clock),
+		.clear_b(Clear_b),
+		.q(Q[5])
 	);
 
 	toggle q6(
-		.t(a[5]);
-		.clock(Clock);
-		.clear_b(Clear_b);
-		.q(Q[6]);
+		.t(a[5]),
+		.clock(Clock),
+		.clear_b(Clear_b),
+		.q(Q[6])
 	);
 
 	toggle q7(
-		.t(a[6]);
-		.clock(Clock);
-		.clear_b(Clear_b);
-		.q(Q[7]);
+		.t(a[6]),
+		.clock(Clock),
+		.clear_b(Clear_b),
+		.q(Q[7])
 	);
 	
 	and_gate a0(
-		.x(Enable);
-		.y(Q[0]);
-		.z(a[0]);	
+		.x(Enable),
+		.y(Q[0]),
+		.z(a[0])	
 	);
 
 	and_gate a1(
-		.x(a[0]);
-		.y(Q[1]);
-		.z(a[1]);	
+		.x(a[0]),
+		.y(Q[1]),
+		.z(a[1])
 	);
 
 	and_gate a2(
-		.x(a[1]);
-		.y(Q[2]);
-		.z(a[2]);	
+		.x(a[1]),
+		.y(Q[2]),
+		.z(a[2])	
 	);
 
 	and_gate a3(
-		.x(a[2]);
-		.y(Q[3]);
-		.z(a[3]);	
+		.x(a[2]),
+		.y(Q[3]),
+		.z(a[3])	
 	);
 
 	and_gate a4(
-		.x(a[3]);
-		.y(Q[4]);
-		.z(a[4]);	
+		.x(a[3]),
+		.y(Q[4]),
+		.z(a[4])	
 	);
 
 	and_gate a5(
-		.x(a[4]);
-		.y(Q[5]);
-		.z(a[5]);	
+		.x(a[4]),
+		.y(Q[5]),
+		.z(a[5])	
 	);
 
 	and_gate a6(
-		.x(a[5]);
-		.y(Q[6]);
-		.z(a[6]);	
+		.x(a[5]),
+		.y(Q[6]),
+		.z(a[6])	
 	);
 
-     	seg0 lower(
+     	seg0 lower0(
         	.c0(Q[0]),
         	.c1(Q[1]),
         	.c2(Q[2]),
         	.c3(Q[3]),
         	.seg0(HEX0[0])
         );
-     	seg1 lower(
+     	seg1 lower1(
         	.c0(Q[0]),
         	.c1(Q[1]),
         	.c2(Q[2]),
         	.c3(Q[3]),
         	.seg1(HEX0[1])
         );
-     	seg2 lower(
+     	seg2 lower2(
         	.c0(Q[0]),
         	.c1(Q[1]),
         	.c2(Q[2]),
         	.c3(Q[3]),
         	.seg2(HEX0[2])
         );
-     	seg3 lower(
+     	seg3 lower3(
         	.c0(Q[0]),
         	.c1(Q[1]),
         	.c2(Q[2]),
         	.c3(Q[3]),
         	.seg3(HEX0[3])
         );
-     	seg4 lower(
+     	seg4 lower4(
         	.c0(Q[0]),
         	.c1(Q[1]),
         	.c2(Q[2]),
         	.c3(Q[3]),
         	.seg4(HEX0[4])
         );
-     	seg5 lower(
+     	seg5 lower5(
         	.c0(Q[0]),
         	.c1(Q[1]),
         	.c2(Q[2]),
         	.c3(Q[3]),
         	.seg5(HEX0[5])
         );
-     	seg6 lower(
+     	seg6 lower6(
         	.c0(Q[0]),
         	.c1(Q[1]),
         	.c2(Q[2]),
@@ -161,56 +161,50 @@ module counter(SW, KEY, HEX0, HEX1);
         	.seg6(HEX0[6])
         );
 
-     	seg0 upper(
+     	seg0 upper0(
         	.c0(Q[4]),
         	.c1(Q[5]),
         	.c2(Q[6]),
         	.c3(Q[7]),
         	.seg0(HEX1[0])
         );
-     	seg1 upper(
+     	seg1 upper1(
         	.c0(Q[4]),
         	.c1(Q[5]),
         	.c2(Q[6]),
         	.c3(Q[7]),
         	.seg1(HEX1[1])
         );
-     	seg1 upper(
-        	.c0(Q[4]),
-        	.c1(Q[5]),
-        	.c2(Q[6]),
-        	.c3(Q[7]),
-        	.seg1(HEX1[1])
-        );
-     	seg2 upper(
+
+     	seg2 upper2(
         	.c0(Q[4]),
         	.c1(Q[5]),
         	.c2(Q[6]),
         	.c3(Q[7]),
         	.seg2(HEX1[2])
         );
-     	seg3 upper(
+     	seg3 upper3(
         	.c0(Q[4]),
         	.c1(Q[5]),
         	.c2(Q[6]),
         	.c3(Q[7]),
         	.seg3(HEX1[3])
         );
-     	seg4 upper(
+     	seg4 upper4(
         	.c0(Q[4]),
         	.c1(Q[5]),
         	.c2(Q[6]),
         	.c3(Q[7]),
         	.seg4(HEX1[4])
         );
-     	seg5 upper(
+     	seg5 upper5(
         	.c0(Q[4]),
         	.c1(Q[5]),
         	.c2(Q[6]),
         	.c3(Q[7]),
         	.seg5(HEX1[5])
         );
-     	seg6 upper(
+     	seg6 upper6(
         	.c0(Q[4]),
         	.c1(Q[5]),
         	.c2(Q[6]),
@@ -224,16 +218,19 @@ endmodule
 module toggle(t, clock, clear_b, q, not_q);
 
 	input t, clock, clear_b;
-	output reg q, not_q;
+	output q, not_q;
+	reg q;
+	wire not_q;
 
 	assign not_q = ~q;
 	
 	always @(posedge clock, negedge clear_b)
 	begin
-		if (clear_b == 1’b0)
+		if (clear_b == 1'b0)
 			q <= 0;
-		if (t == 1’b1)
-			q <= ~q;
+		else 
+			if (t == 1'b1)
+				q <= ~q;
 	end
 endmodule
 
@@ -253,7 +250,7 @@ module seg0(c0, c1, c2, c3, seg0);
     output seg0;
     assign seg0 = (~c3 & ~c2 & ~c1 & c0) | (~c3 & c2 & ~c1 & ~c0)
     | (c3 & c2 & ~c1 & c0) | (c3 & ~c2 & c1 &c0);
-endmodule;
+endmodule
 
 module seg1(c0, c1, c2, c3, seg1);
     input c0;
@@ -266,7 +263,7 @@ module seg1(c0, c1, c2, c3, seg1);
     (c3 & c1 & c0) |
     (c2 & c1 & ~c0) |
     (c3 & c2 & c1);
-endmodule;
+endmodule
 
 module seg2(c0, c1, c2, c3, seg2);
     input c0;
@@ -277,7 +274,7 @@ module seg2(c0, c1, c2, c3, seg2);
     assign seg2 = (c3 & c2 & ~c1 & ~c0) |
     (~c3 & ~c2 & c1 & ~c0) |
     (c3 & c2 & c1);
-endmodule;
+endmodule
 
 module seg3(c0, c1, c2, c3, seg3);
     input c0;
@@ -289,7 +286,7 @@ module seg3(c0, c1, c2, c3, seg3);
     (!c3 & ~c2 & ~c1 & c0) |
     (c2 & c1 & c0) |
     (c3 & ~c2 & c1 & ~c0);
-endmodule;
+endmodule
 
 module seg4(c0, c1, c2, c3, seg4);
     input c0;
@@ -300,7 +297,7 @@ module seg4(c0, c1, c2, c3, seg4);
     assign seg4 = (~c3 & c0) |
     (~c3 & c2 & ~c1) |
     (~c2 & ~c1 & c0);
-endmodule;
+endmodule
 
 module seg5(c0, c1, c2, c3, seg5);
     input c0;
@@ -311,7 +308,7 @@ module seg5(c0, c1, c2, c3, seg5);
     assign seg5 = (~c3 & ~c2 & c0) |
     (~c3 & c1 & c0) |
     (~c3 & ~c2 & c1);
-endmodule;
+endmodule
 
 module seg6(c0, c1, c2, c3, seg6);
     input c0;
@@ -322,4 +319,4 @@ module seg6(c0, c1, c2, c3, seg6);
     assign seg6 = (~c3 & ~c2 & ~c1) |
     (c3 & c2 & ~c1 & ~c0) |
     (~c3 & c2 & c1 & c0);
-endmodule;
+endmodule
